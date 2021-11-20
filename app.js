4148e2bf49f58a73b679ab050c59114c4baaa51e
Katch <katchvidal@gmail.com>
2021-11-19 21:24:19 -0600

    web.config azure deploy

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-const { ApolloServer } = require('apollo-server');
-const JWT = require('jsonwebtoken')
-require('dotenv').config()
-const { resolvers } = require('./GraphQL/resolvers')
-const { typeDefs } = require('./GraphQL/typeDefs')
-const { basedatos } = require('./database/config.db');
+const { ApolloServer } = require("apollo-server");
+const JWT = require("jsonwebtoken");
+require("dotenv").config();
+const { resolvers } = require("./GraphQL/resolvers");
+const { typeDefs } = require("./GraphQL/typeDefs");
+const { basedatos } = require("./database/config.db");
 
 //  Conexion a la base de datos
 basedatos();
 
-
 //  Crear Servidor ->
 const server = new ApolloServer({
-    cors,
-    typeDefs,
-    resolvers,
-    context: ({ req }) => {
-        const token = req.headers['token'] || '';
-        if( token ){
-            try {
-
-                const usuario = JWT.verify( token, process.env.SECRETORPRIVATEKEY );
-                return usuario;
-                
-            } catch (error) {
-                console.log( error );
-                console.log('token no valido / vencido ');
-            }
-        };
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    const token = req.headers["token"] || "";
+    if (token) {
+      try {
+        const usuario = JWT.verify(token, process.env.SECRETORPRIVATEKEY);
+        return usuario;
+      } catch (error) {
+        console.log(error);
+        console.log("token no valido / vencido ");
+      }
     }
+  },
 });
 
 //  Arrancar Servidor ->
-server.listen({port: process.env.PORT || 4000 }).then(( {url} ) =>{
-    console.log('Servidor Corriendo en la URL', url );
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log("Servidor Corriendo en la URL", url);
 });
